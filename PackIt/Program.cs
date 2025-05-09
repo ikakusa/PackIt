b7d237ed44f39b148f3d944706256b6b1f276dff
誠 遺憾 <ikasan2860@gmail.com>
2025-05-10 01:36:19 +0900

    プロジェクト ファイルを追加します。

---
+++ PackIt/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PackIt
+{
+    internal class Program
+    {
+        static Random random = new Random();
+        static string get_random(int length)
+        {
+            string strs = "abcdefghijklmnopqrstuvwxyz1234567890";
+            StringBuilder sb = new StringBuilder(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                int rnd = random.Next(strs.Length);
+                char c = strs[rnd];
+                sb.Append(c);
+            }
+            return sb.ToString();
+        }
+        static void Main(string[] args)
+        {
+            string outputPath = Directory.GetParent(args[0]).FullName;
+            string path = Path.GetFullPath(Path.Combine(outputPath, $"new_pack"));
+            Directory.CreateDirectory(path);
+            foreach (string arg in args)
+            {
+                string name = Path.GetFileName(arg);
+                File.Copy(arg, Path.GetFullPath(Path.Combine(path, name)));
+            }
+        }
+    }
+}
---
+++ PackItSetup/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace PackItSetup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string appdata = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string appPath = $@"{appdata}\Programs\Ikakusa";
+            if (!Directory.Exists(appPath))
+            {
+                Directory.CreateDirectory(appPath);
+            }
+            File.Copy($@"{Environment.CurrentDirectory}\PackIt.exe", $@"{appPath}\PackIt.exe", true);
+            string path = $@"{appPath}\PackIt.exe";
+            RegistryKey key1 = Registry.ClassesRoot.CreateSubKey(@"*\shell\PackIt");
+            key1.SetValue("", "Pack to a folder", RegistryValueKind.ExpandString);
+            key1.SetValue("Icon", path, RegistryValueKind.ExpandString);
+
+            RegistryKey key2 = Registry.ClassesRoot.CreateSubKey(@"*\shell\PackIt\command");
+            key2.SetValue("", $"\"{path}\" %1", RegistryValueKind.ExpandString);
+
+            key2.Close();
+            key1.Close();
+        }
+    }
+}
